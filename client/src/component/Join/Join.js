9fa10bda60876b0e62bf7171031049bad8a4c186
Arghadeep Mallick <66899260+Arghadeep-hub@users.noreply.github.com>
2021-05-19 19:15:57 +0530

    Update Join.js
    Updated UI

--- client/src/component/Join/Join.js
+++ client/src/component/Join/Join.js
@@ -6,12 +6,19 @@ import "./Join.css";
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import LaptopIcon from '@material-ui/icons/Laptop';
-
-import avatar from '../../img/avatar.svg';
 import bg from "../../img/bg.svg";
 import wave from "../../img/wave.png";
 
+import "swiper/swiper.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { EffectCoverflow, Pagination, Navigation } from "swiper/core";
+
+SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
+
 const ENDPOINT = 'http://localhost:5000';
+
 
 const Join = () => {
   let history = useHistory();
@@ -20,28 +27,29 @@ const Join = () => {
   const [about, setAbout] = useState("");
   const [location, setLocation] = useState({});
   const [namespaceId, setNamespaceId] = useState(0);
-  const [maxCount, setMaxCount] = useState(100);
-  const [count, setCount] = useState(0);
+  const [maxCount] = useState(100);
+  const [count, setCount] = useState(10);
 
   useEffect(() => {
-    
-    if ('geolocation'  in navigator) {
+
+    if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords);
-      setLocation({"long":position.coords.longitude, "lat":position.coords.latitude});
-      fetch(`${ENDPOINT}/check?long=${position.coords.longitude}&lat=${position.coords.latitude}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (result.status === 'success')
-            setNamespaceId(result.data.id)
-        }
-      )});
+        console.log(position.coords);
+        setLocation({ "long": position.coords.longitude, "lat": position.coords.latitude });
+        fetch(`${ENDPOINT}/check?long=${position.coords.longitude}&lat=${position.coords.latitude}`)
+          .then(res => res.json())
+          .then(
+            (result) => {
+              if (result.status === 'success')
+                setNamespaceId(result.data.id)
+            }
+          )
+      });
     }
     else {
       alert("please allow location access")
     }
-  }, [navigator.geolocation]);  
+  }, []);
 
 
   const checkNull = async (event) => {
@@ -49,11 +57,11 @@ const Join = () => {
       alert("Please fill in your Details");
       event.preventDefault();
     }
-    else if(!('long' in location)) {
+    else if (!('long' in location)) {
       alert("Please allow location access");
       event.preventDefault();
     }
-    else if(namespaceId === 0) {
+    else if (namespaceId === 0) {
       alert("Sorry! You're not near any Chat area");
       event.preventDefault();
     }
@@ -62,12 +70,12 @@ const Join = () => {
       const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 'name':name, 'profession':profession, 'about':about, 'namespaceId':namespaceId })
+        body: JSON.stringify({ 'name': name, 'profession': profession, 'about': about, 'namespaceId': namespaceId })
       };
 
       let res = await fetch(`${ENDPOINT}/addDetails`, requestOptions)
       let result = await res.json()
-      if (result.status == "success"){
+      if (result.status === "success") {
         console.log(result.data);
         history.push(`/chat?userId=${result.data}`)
       }
@@ -78,7 +86,7 @@ const Join = () => {
 
   useEffect(() => {
     setCount(maxCount - about.length);
-  }, [about])
+  }, [about, maxCount])
 
   const inputs = document.querySelectorAll(".input");
   function addClass() {
@@ -107,7 +115,32 @@ const Join = () => {
         </div>
         <div className="login-content">
           <div className="form">
-            <img src={avatar} alt="img" />
+            <div className="section">
+              <Swiper effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                loop={true}
+                coverflowEffect={{
+                  'rotate': 0,
+                  'stretch': 30,
+                  'depth': 100,
+                  'modifier': 6,
+                  'slideShadows': false
+                }}
+                navigation={false}
+                className="mySwiper">
+                <div class="swiper-wrapper">
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/initials/${name}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/avataaars/${name[0]}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/micah/${profession}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/gridy/${profession[0]}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/female/${name}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/male/${name}.svg`} alt="" /></SwiperSlide>
+                  <SwiperSlide><img src={`https://avatars.dicebear.com/api/bottts/${name[1]}.svg`} alt="" /></SwiperSlide>
+                </div>
+              </Swiper>
+            </div>
             <h2 className="title">Your Details</h2>
             <div className="input-div one">
               <div className="i">
@@ -144,12 +177,13 @@ const Join = () => {
                   spellcheck="True"
                   maxLength={maxCount}
                   onChange={(event) => setAbout(event.target.value)}
+                  style={count <= 50 ? count <= 10 ? { color: "#ff5722" } : { color: "#4caf50" } : null}
                 />
-                <p className="show">{count}</p>
+                <p className="show" style={count <= 50 ? count <= 10 ? { color: "#ff5722", fontWeight: "bolder" } : { color: "#4caf50", fontWeight: "bolder" } : null}>{count}</p>
               </div>
             </div>
-              <button className="btn" type="button" onClick={(event) =>checkNull(event)}>
-                Proceed
+            <button className="btn" type="button" onClick={(event) => checkNull(event)}>
+              Proceed
               </button>
           </div>
         </div>
